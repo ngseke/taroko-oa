31713b2b68925ffdbccbbdbb4bb582771aee49bd
ngseke <ngseke@gmail.com>
2023-12-07 15:46:08 +0800

    feat: create types

---
+++ src/types/Contact.ts
@@ -0,0 +1,14 @@
+export interface Contact {
+  id: number
+  first_name: string
+  last_name: string
+  job: string
+  description: string
+}
+
+export interface ContactDraft {
+  firstName: string
+  lastName: string
+  job: string
+  description: string
+}
