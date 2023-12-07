ebbfcdb93fcde700e61d4a44e2b7018a302177d7
ngseke <ngseke@gmail.com>
2023-12-07 15:21:29 +0800

    feat: create component ContactCards

---
+++ src/components/ContactCards.tsx
@@ -0,0 +1,31 @@
+import { type Contact } from '../types/Contact'
+import { ContactCard } from './ContactCard'
+
+export interface ContactCardsProps {
+  contacts?: Contact[]
+  onClickEdit?: (id: number) => void
+  onClickDelete?: (id: number) => void
+}
+
+export function ContactCards ({
+  contacts,
+  onClickEdit,
+  onClickDelete,
+}: ContactCardsProps) {
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      {
+        contacts?.map(contact => (
+          <ContactCard
+            key={contact.id}
+            name={`${contact.first_name} ${contact.last_name}`}
+            job={contact.job}
+            description={contact.description}
+            onClickEdit={() => onClickEdit?.(contact.id)}
+            onClickDelete={() => onClickDelete?.(contact.id)}
+          />
+        ))
+      }
+    </div>
+  )
+}
