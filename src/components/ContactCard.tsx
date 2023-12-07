865e90d2282db5c63c3ad07c6ae5d3ce3a16f63a
ngseke <ngseke@gmail.com>
2023-12-07 13:15:17 +0800

    feat: create component ContactCardMeta

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,47 @@
+import { Button } from './Button'
+
+function ContactCardMeta (
+  { label, value }: Partial<Record<'label' | 'value', string>>
+) {
+  return (
+    <div className="flex gap-2 text-sm">
+      <span className="font-medium text-neutral-600">{label}: </span>
+      <span>{value}</span>
+    </div>
+  )
+}
+
+export interface ContactCardProps {
+  name?: string
+  job?: string
+  description?: string
+
+  onClickEdit?: () => void
+  onClickDelete?: () => void
+}
+
+export function ContactCard ({ name, job, description, onClickEdit, onClickDelete }: ContactCardProps) {
+  const avatarUrl = `https://api.dicebear.com/7.x/adventurer-neutral/svg?seed=${name}
+  `
+
+  return (
+    <div className="rounded-xl border border-neutral-300 p-4">
+      <div className="grid grid-cols-[1fr,auto] grid-rows-[1fr,auto] gap-4 md:grid-cols-[auto,1fr]">
+        <div className="flex items-center gap-2">
+          <img src={avatarUrl} alt="Avatar" className="h-8 w-8 rounded-full" />
+          <span className="font-bold">{name}</span>
+        </div>
+
+        <div className="flex flex-none flex-col gap-2 md:row-span-2">
+          <Button onClick={onClickEdit}>Edit</Button>
+          <Button color="danger" onClick={onClickDelete}>Delete</Button>
+        </div>
+
+        <div className="col-span-2 flex flex-col gap-1 md:col-span-1">
+          <ContactCardMeta label="Job" value={job} />
+          <ContactCardMeta label="Description" value={description} />
+        </div>
+      </div>
+    </div>
+  )
+}
